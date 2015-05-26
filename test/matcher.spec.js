09cc0d75f5bbbaff6134ea3bccc7c669de881e79
Zekai.Zheng <kiddkai@gmail.com>
2015-05-26 12:14:48 +1000

    named schema for generating JSON Schema

--- test/matcher.spec.js
+++ test/matcher.spec.js
@@ -1,25 +1,46 @@
-var inherits = require('util').inherits;
 var Matcher  = require('../lib/matcher');
 var factory  = require('../lib/factory');
 
-describe('Matcher',function(){
+describe('Matcher', function() {
 
-  describe('is',function(){
+  describe('is', function() {
 
-    it('returns false when it is compared to null',function(){
+    it('returns false when it is compared to null', function() {
       Matcher.is(null).should.eql(false);
     });
 
-    it('returns true when a strummer matcher is passed',function(){
+    it('returns true when a strummer matcher is passed', function() {
       var DummyMatcher = factory({
-        match: function(){
+        match: function() {
           return false;
         }
       });
 
-      inherits(DummyMatcher,Matcher)
-
       Matcher.is(new DummyMatcher()).should.eql(true);
+    });
+
+  });
+
+  describe('#toJSONSchema', function() {
+
+    it('generates named json schema when it have name', function() {
+      var DummyMatcher = factory({
+        match: function() {
+          return true;
+        },
+        toJSONSchema: function() {
+          return {
+            type: 'number'
+          };
+        }
+      });
+
+      var m = new DummyMatcher();
+      m.setName('Dummy');
+      m.toJSONSchema().should.eql({
+        name: 'Dummy',
+        type: 'number'
+      });
     });
 
   });
